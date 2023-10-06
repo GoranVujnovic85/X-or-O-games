2719215d689e20c1ef64ace6fc954befee134c65
Goran <goranvujnovic85@gmail.com>
2023-10-06 05:57:43 +0200

    second commit

--- Main.c
+++ Main.c
@@ -15,6 +15,8 @@ Description :This program is a game of X or O between two users
 /* Declaration of global variables */
 char matrix [3][3];
 int currentUser = 2;
+int moves = 0;
+int maxMoves = 9;
 
 
 /* This function creates a table and fills it * */
@@ -61,109 +63,107 @@ void switchUser()
 }
 
 
-/* This function this function checks by rows, columns and diagonally
-    whether he entered three identical characters and won the game    */
+/*This function this function checks by rows, columns and diagonally
+whether he entered three identical characters and won the game.*/
 int winner()
 {
-	for (int i = 0; i < 3; i++)
+	for(int i = 0; i < 3; i++)
 	{
-		if (matrix[i][0] == matrix[i][1] && matrix[i][1] == matrix[i][2] && matrix[i][0]  == 'X')
+		if((matrix[i][0] == matrix[i][1] && matrix[i][1] == matrix[i][2]) || (matrix[0][i] == matrix[1][i] && matrix[1][i] == matrix[2][i]))
 		{
-			return 'X';
-		}
-		if (matrix[i][0] == matrix[i][1] && matrix[i][1] == matrix[i][2] && matrix[i][0] == '0')
-		{
-			return '0';
-		}
-		if (matrix[0][i] == matrix[1][i] && matrix[1][i] == matrix[2][i] && matrix[0][i] == 'X')
-		{
-			return 'X';
-		}
-		if (matrix[0][i] == matrix[1][i] && matrix[1][i] == matrix[2][i] && matrix[0][i] == '0')
-		{
-			return '0';
+			if(matrix[i][0] == 'X')
+			{
+				return 'X';
+			}
+			else if(matrix[i][0] == '0')
+			{
+				return '0';
+			}
 		}
 	}
-	if (matrix[0][0] == matrix[1][1] && matrix[1][1] == matrix[2][2] && matrix[0][0] == 'X')
-	{
-		return 'X';
-	}
-	if (matrix[0][0] == matrix[1][1] && matrix[1][1] == matrix[2][2] && matrix[0][0] == '0')
-	{
-		return '0';
-	}
-	if (matrix[0][2] == matrix[1][1] && matrix[1][1] == matrix[2][0] && matrix[0][2] == 'X')
-	{
-		return 'X';
-	}
-	if (matrix[0][2] == matrix[1][1] && matrix[1][1] == matrix[2][0] && matrix[0][2] == '0')
-	{
-		return '0';
-	}
+		if((matrix[0][0] == matrix[1][1] && matrix[1][1] == matrix[2][2]) || (matrix[0][2] == matrix[1][1] && matrix[1][1] == matrix[2][0]))
+		{
+			/*Matrix[1][1] represents the center field of the matrix which is
+			essential for checking the diagonals and deciding the winner of the game.*/
+			if(matrix[1][1] == 'X')
+			{
+				return 'X';
+			}
+			else if(matrix[1][1] == '0')
+			{
+				return '0';
+			}
+		}
 
 	return 0;
 }
 
 
-/* This function requires input from the user */
-void makeMove(int player)
+/*Check the winner*/
+void checkTheWiner()
 {
-	int choice;
+	int Winner = winner();
 
-	printf("User %d can choose the number from 1 to 9 that make move: \n\n", player);
+	if(Winner)
+	{
+		printf("\tPlayer %c wins!\n\n", (currentUser == 1) ? 'X' : 'O');
+	}
+	else if(moves == maxMoves)
+	{
+		printf("\tThere are no winners!\n");
+	}
+}
+
+
+/* This function requires input from the user */
+void makeMove(int currentUser)
+{
+	int choice = 2;
+
+	printf("User %d can choose the number from 1 to 9 that make move: \n\n", currentUser);
 	fflush(stdout);
 	scanf("%d", &choice);
 
-	/*https://www.youtube.com/watch?v=zH_omFPqMO4*/
+	/*After subtracting 1, the choice value is divided by 3 using the / operator.
+	This is done to determine what type or row that position is in the game.*/
 	int i = (choice - 1) / 3;
+	/*After subtracting 1, the choice value is divided by 3 using the % operator (modulo operation).
+	The modulo operation returns the remainder of dividing a number by another number.
+	In this case, choice - 1 is divided by 3, and j will contain the remainder of that division.
+	Example:
+	The expressions 0 % 3 and 1 % 3 yield 0 and 1, because these numbers are partially divisible by 3 with no remainder.
+    The expression 2 % 3 gives 2, because 2 is not divisible by 3 and the remainder is 2 itself.*/
 	int j = (choice - 1) % 3;
 
 	if (matrix[i][j] == '*')
 	{
 		matrix[i][j] = (currentUser == 1) ? 'X' : '0';
+		tablePrint();
 	}
 
 	else
 	{
 		printf("The field is already occupied. Select a new value.\n\n");
 		fflush(stdout);
-		makeMove(player);
 	}
-		tablePrint();
-
-		int Winner = winner();
-
-		if (Winner)
-		{
-		    printf("\tPlayer %c wins!\n\n", (currentUser == 1) ? 'X' : 'O');
-		    return;
-		}
 }
+
 
 /* Main function */
 int main()
 {
 	printf("\tThis is your board.\n\n");
+	printf("You can start with moves and good luck!\n\n");
 	tableCreating();
 	tablePrint();
-	printf("\n");
-	printf("\tYou can start with moves and good luck!\n\n");
-
-	int moves = 0;
-	int maxMoves = 9;
 
 	while(moves < maxMoves)
 	{
 		switchUser();
 		makeMove(currentUser);
 		moves++;
-
-		if (moves == maxMoves)
-		{
-			printf("\tThere are no winners!\n");
-		}
+		checkTheWiner();
 	}
-
 	return 0;
 }
 
